689053c3cc63572c2301af6eb8abe282fc6e60e9
Jonathan T <thickens@wisc.edu>
2019-10-30 01:58:56 -0500

    Beginning of refactor to integrate with Firebase Auth

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:project/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    //if auth return home or auth widget
+
+    return Home();
+  }
+}
