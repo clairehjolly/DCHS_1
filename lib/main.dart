1a9c900af735a6029d0861b515d07d073f3f3f24
Jonathan T <thickens@wisc.edu>
2019-10-29 20:49:54 -0500

    Getting closer on UI. Username/password fields configured, button UI elem configured. Next step will be to plum in firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,136 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("Dane County Humane Society"),
-      centerTitle: true,
-      backgroundColor: Colors.purple[800],
-    ),
-    body: Center (
-      child: Column(
-        children: [
-          Text("Login/Signup page"),
-          Text("email"),
-          Text("password"),
-          Text("username"),
-          Text("Test change for remote"),
-        ],
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-    )
-  ),
-));
+      home: MyHomePage(title: 'Flutter Login'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextStyle style = TextStyle(fontSize: 20.0);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final emailField = TextField(
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Email",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final passwordField = TextField(
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final loginButon = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {},
+        child: Text("Login",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    return Scaffold(
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(36.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 155.0,
+                    ),
+                    SizedBox(height: 45.0),
+                    emailField,
+                    SizedBox(height: 25.0),
+                    passwordField,
+                    SizedBox(
+                      height: 35.0,
+                    ),
+                    loginButon,
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        )
+    );
+  }
+}
+
+//void main() => runApp(MaterialApp(
+//  home: Scaffold(
+//    appBar: AppBar(
+//      title: Text("Dane County Humane Society"),
+//      centerTitle: true,
+//      backgroundColor: Colors.purple[800],
+//    ),
+//    body: Column(
+//        children: [
+//          TextFormField(
+//            decoration: InputDecoration(
+//              labelText: 'Enter your email'
+//            ),
+//          ),
+//          TextFormField(
+//            decoration: InputDecoration(
+//                labelText: 'Enter your password'
+//            ),
+//          ),
+//        ],
+//    ),
+//
+//
+//
+//    floatingActionButton: FloatingActionButton(
+//      onPressed: () {},
+//      child: Text("Button"),
+//      backgroundColor: Colors.purple[800],
+//    ),
+//  ),
+//));
+
