84652c6247d2787f09af9235ddd12a2defbbed81
trishabernice <44876794+trishabernice@users.noreply.github.com>
2019-10-31 13:18:52 -0500

    changed Admin page UI + settings page UI

--- lib/screens/home/menu_pages/admin.dart
+++ lib/screens/home/menu_pages/admin.dart
@@ -3,146 +3,70 @@ import 'package:flutter/material.dart';
 class Admin extends StatelessWidget {
   @override
   Widget build(BuildContext context){
-    Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Center(
           child:Text(
             'Admin',
             style: TextStyle(
               fontFamily: 'Bitter',
+              fontSize: 25.0,
             ),
           ),
         ),
         backgroundColor: Color(0xff6b2978),
 
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-            //color: Color(0xffffc50d),
-            child: Text(
-              'ANIMAL',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 22.0,
-                fontFamily: 'Bitter',
+      body: new Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+
+          children: <Widget>[
+            FlatButton.icon(
+              onPressed: () {},
+              color: Color(0xffffc50d),
+              icon: Icon(Icons.pets),
+              label: Text(
+                'Animals',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontFamily: 'Bitter',
+                ),
               ),
             ),
 
-          ),
-          FlatButton(
-            onPressed: () {},
-            color: Color(0xffffc50d),
-            child: Text(
-              'Edit',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontFamily: 'Bitter',
-              ),
-            ),
-          ),
-          FlatButton(
-            onPressed: () {},
-            color: Color(0xffffc50d),
-            child: Text(
-              'Add',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontFamily: 'Bitter',
-              ),
-            ),
-          ),
-          FlatButton(
-            onPressed: () {},
-            color: Color(0xffffc50d),
-            child: Text(
-              'Delete',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontFamily: 'Bitter',
-              ),
-            ),
-          ),
 
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-            // color: Color(0xffaa295d),
-            child: Text(
-              'SERVICES',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 22.0,
-                fontFamily: 'Bitter',
+            FlatButton.icon(
+              onPressed: () {},
+              color: Color(0xffaa295d),
+              icon: Icon(Icons.menu),
+              label: Text(
+                'Services',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontFamily: 'Bitter',
+                ),
               ),
             ),
 
-          ),
-          FlatButton(
-            onPressed: () {},
-            color: Color(0xffaa295d),
-            child: Text(
-              'Edit',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontFamily: 'Bitter',
-              ),
-            ),
-          ),
-
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-            //color: Color(0xff96be04),
-            child: Text(
-              'SCHEDULE',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 22.0,
-                fontFamily: 'Bitter',
+            FlatButton.icon(
+              onPressed: () {},
+              color: Color(0xff96be04),
+              icon: Icon(Icons.calendar_today),
+              label: Text(
+                'Schedules',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontFamily: 'Bitter',
+                ),
               ),
             ),
 
-          ),
-          FlatButton(
-            onPressed: () {},
-            color: Color(0xff96be04),
-            child: Text(
-              'Edit',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontFamily: 'Bitter',
-              ),
-            ),
-          ),
-          FlatButton(
-            onPressed: () {},
-            color: Color(0xff96be04),
-            child: Text(
-              'Add',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontFamily: 'Bitter',
-              ),
-            ),
-          ),
-          FlatButton(
-            onPressed: () {},
-            color: Color(0xff96be04),
-            child: Text(
-              'Delete',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontFamily: 'Bitter',
-              ),
-            ),
-          ),
 
+          ],
 
-
-        ],
-
+        ),
       ),
     );
   }
--- lib/screens/home/menu_pages/settings.dart
+++ lib/screens/home/menu_pages/settings.dart
@@ -3,69 +3,72 @@ import 'package:flutter/material.dart';
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Center(
           child:Text(
             'Settings',
             style: TextStyle(
               fontFamily: 'Bitter',
+              fontSize: 25.0,
             ),
           ),
         ),
         backgroundColor: Color(0xff6b2978),
 
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
 
-          FlatButton.icon(
-            onPressed: () {}, //go to user profile
-            color: Color(0xffffc50d),
-            icon: Icon(Icons.people),
-            label: Text(
-              'Edit Profile',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontFamily: 'Bitter',
+            FlatButton.icon(
+              onPressed: () {}, //go to user profile
+              color: Color(0xffffc50d),
+              icon: Icon(Icons.people),
+              label: Text(
+                'Edit Profile',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontFamily: 'Bitter',
+                ),
+              ),
+
+            ),
+
+
+            FlatButton.icon(
+              onPressed: () {}, //turn grey if on
+              color: Color(0xffaa295d),
+              icon: Icon(Icons.notifications_active),
+              label: Text(
+                'Notifications',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontFamily: 'Bitter',
+                ),
               ),
             ),
 
-          ),
 
-
-          FlatButton.icon(
-            onPressed: () {}, //turn grey if on
-            color: Color(0xffaa295d),
-            icon: Icon(Icons.notifications_active),
-            label: Text(
-              'Notifications',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontFamily: 'Bitter',
+            FlatButton.icon(
+              onPressed: () {}, //Direct user to About This APP View
+              color: Color(0xff96be04),
+              icon: Icon(Icons.assistant),
+              label: Text(
+                'Version, Support & Privacy',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontFamily: 'Bitter',
+                ),
               ),
             ),
-          ),
 
 
-          FlatButton.icon(
-            onPressed: () {}, //Direct user to About This APP View
-            color: Color(0xff96be04),
-            icon: Icon(Icons.assistant),
-            label: Text(
-              'Version, Support & Privacy',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontFamily: 'Bitter',
-              ),
-            ),
-          ),
+          ],
 
-
-        ],
-
+        ),
       ),
     );
 
