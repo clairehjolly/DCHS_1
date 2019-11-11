903ea1d28ba916b35ca2c7620e3795d01814aea7
Yu-Tai Chen <32503459+kevchen1129@users.noreply.github.com>
2019-11-10 23:09:35 -0600

    text and drop down button work seperately

--- lib/screens/home/adopt.dart
+++ lib/screens/home/adopt.dart
@@ -41,8 +41,15 @@ class _AnimalsListPageState extends State<AnimalsListPage> {
 
   List<NewAnimal> animal;
   List<NewAnimal>filterAnimal;
+  List<NewAnimal>temp_name;
+  List<NewAnimal>temp_age;
+
   FirestoreService fs = new FirestoreService();
   StreamSubscription<QuerySnapshot> animalView;
+  String _selectedAge=null;
+  String _selectedGender=null;
+  String _selectedSpecies=null;
+
 
   @override
   void initState(){
@@ -59,35 +66,146 @@ class _AnimalsListPageState extends State<AnimalsListPage> {
       setState(() {
         this.animal = animals;
         this.filterAnimal=animal;
+        this.temp_name=animal;
+        this.temp_age=animal;
       });
     });
   }
+  List<NewAnimal> checkfilter(List <NewAnimal>filterAnimal,List<NewAnimal> temp){
+    List<NewAnimal>res;
+    for(int i=0;i<filterAnimal.length;i++){
+     var item=filterAnimal.elementAt(i);
+     if(temp.contains(item)){
+       res.add(item);
+     }
+    }
+    //List<NewAnimal>temp=animal;
 
+
+
+    return res;
+  }
+  List<DropdownMenuItem<String>> _dropDownItem(){
+    var _age=['1','2','3','4','5','6','7','8','9','10','11','12'];
+    return _age.map(
+            (value)=>DropdownMenuItem(
+          value: value,
+          child: Text(value),
+        )
+    ).toList();
+  }
+  List<DropdownMenuItem<String>> _dropDownItem1(){
+    var _gender=['Male','Female'];
+    return _gender.map(
+            (value)=>DropdownMenuItem(
+          value: value,
+          child: Text(value),
+        )
+    ).toList();
+  }
+  List<DropdownMenuItem<String>> _dropDownItem2(){
+    var _Species=['Dog','Cat'];
+    return _Species.map(
+            (value)=>DropdownMenuItem(
+          value: value,
+          child: Text(value),
+        )
+    ).toList();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("RRRRR"),
+        title: Text("Adopt Page"),
       ),
       body: Column(
         children: <Widget>[
           TextField(
+
             decoration: InputDecoration(
               contentPadding: EdgeInsets.all(15.0),
-              hintText: 'Filter by name or email',
+              hintText: 'Enter Name',
             ),
             onChanged: (string) {
 
               setState(() {
-                filterAnimal = animal
-                    .where((u) => (u.name
-                    .toLowerCase()
-                    .contains(string.toLowerCase())))
-                    .toList();
+                temp_name=filterAnimal;
+              //  if(string.isNotEmpty) {
+                  filterAnimal = animal
+                      .where((u) => (u.name
+                      .toLowerCase()
+                      .contains(string.toLowerCase())))
+                      .toList();
+                 // filterAnimal=checkfilter(filterAnimal,temp_name);
+             //   }
               });
 
             },
           ),
+      DropdownButton(
+          value: _selectedAge,
+          items: _dropDownItem(),
+          onChanged: (value){
+          _selectedAge=value;
+                setState(() {
+                  temp_age=filterAnimal;
+
+          filterAnimal = animal
+          .where((u) => (u.age.toLowerCase()
+          .contains(value.toLowerCase())))
+           .toList();
+  //  }
+       });
+      },
+       hint: Text('Select Age'),
+       ),
+          DropdownButton(
+            value: _selectedSpecies,
+            items: _dropDownItem2(),
+            onChanged: (value){
+              _selectedSpecies=value;
+              setState(() {
+                //  if(_selectedAge.isNotEmpty){
+                filterAnimal = animal
+                    .where((u) => (u.species.toLowerCase()
+                    .contains(value.toLowerCase())))
+                    .toList();
+                //  filterAnimal=checkfilter(filterAnimal,temp_age);
+                //  }
+              });
+            },
+            hint: Text('Select Species'),
+          ),
+          DropdownButton(
+            value: _selectedGender,
+            items: _dropDownItem1(),
+            onChanged: (value){
+              _selectedGender=value;
+              setState(() {
+                filterAnimal = animal
+                    .where((u) => (u.sex.toLowerCase()
+                    .contains(value.toLowerCase())))
+                    .toList();
+              });
+            },
+            hint: Text('Select Gender'),
+          ),
+//            decoration: InputDecoration(
+//              contentPadding: EdgeInsets.all(15.0),
+//              hintText: 'ENTER AGE',
+//            ),
+//            onChanged: (string) {
+//
+//              setState(() {
+//                filterAnimal = animal
+//                    .where((u) => (u.age
+//                    .toLowerCase()
+//                    .contains(string.toLowerCase())))
+//                    .toList();
+//              });
+//
+//            },
+
           Expanded(
             child: ListView.builder(
               padding: EdgeInsets.all(10.0),
