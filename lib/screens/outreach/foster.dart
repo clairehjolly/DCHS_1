7ccefcece8b5e2ce1d52f4eafafe4b686427eb17
clairehjolly <clairehjolly@gmail.com>
2019-11-12 14:11:16 -0600

    Added notifications and foster page

--- lib/screens/outreach/foster.dart
+++ lib/screens/outreach/foster.dart
@@ -25,7 +25,7 @@ class Foster extends StatelessWidget {
               Container(
                 color: Color(0xffffc50d),
                 child: Text(
-                  'to do: Helpful information, foster description( what do fosters do), Application process, Link to website for more application details ',
+                  'to do: Helpful information, foster description(  what do fosters do), Application process, Link to website for more application details ',
                   style: TextStyle(
                     fontFamily: 'Bitter',
                     fontSize: 20.0,
