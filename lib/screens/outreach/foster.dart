745aad09eba148af4969c4a7c1fa3d8c9ba8445d
clairehjolly <clairehjolly@gmail.com>
2019-11-12 23:28:57 -0600

    Foster page with non functional button - Problem: only way to exit app to a  open a web link is through a 'dead' dependency called url_launcher.  installing the dependency/ plug in causes MASSIVE Errors in build time

--- lib/screens/outreach/foster.dart
+++ lib/screens/outreach/foster.dart
@@ -21,18 +21,38 @@ class Foster extends StatelessWidget {
       ),
       body: new Center(
         child: Column(
-            children: <Widget>[
-              Container(
-                color: Color(0xffffc50d),
-                child: Text(
-                  'to do: Helpful information, foster description(  what do fosters do), Application process, Link to website for more application details ',
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+
+              color: Colors.amber,
+              child: Text('If you would like to help DCHS but you cannot fit into your lifestyle the'+
+                  'traditional DCHS volunteer commitment to serve a regular weekly,'+
+                  ' two hour shift for six months, you may be interested in the following opportunity'+
+                  ' \n\nProvide a home for animals too young to be adopted, pregnant or new moms'+
+                  ' those in need of extra socialization, those overly stressed at the shelter and those'+
+                  'recovering from illness or injury',
                   style: TextStyle(
-                    fontFamily: 'Bitter',
-                    fontSize: 20.0,
-                  ),
+                  fontFamily: 'Bitter',
+                  fontSize: 14.0,
+              )),
+            ),
+
+
+            FlatButton.icon(
+              onPressed: () {}, //Direct user to About This APP View
+              color: Color(0xff96be04),
+              icon: Icon(Icons.assistant),
+              label: Text(
+                'Application  form',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontFamily: 'Bitter',
                 ),
               ),
-            ]
+            ),
+          ],
         ),
       ),
     );
