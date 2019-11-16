51b39afcc89f984b90bc5473e4dd2f4f9f15c71c
trishabernice <44876794+trishabernice@users.noreply.github.com>
2019-11-15 21:21:49 -0600

    Update animalProfile.dart

--- lib/screens/admin/animalProfile.dart
+++ lib/screens/admin/animalProfile.dart
@@ -1,122 +1,156 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:project/screens/admin/animals.dart';
 
-// This shows the list of information of Animal
 
 class AnimalProfile extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
+    return new Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Center(
           child: Text(
             'Animal Profile',
             style: TextStyle(
               fontFamily: 'Bitter',
               fontSize: 25.0,
-            ), //TextStyle
+            ),
           ),
         ),
-        backgroundColor: Color(0xff6b2978),
+        backgroundColor: Color(0xffaa295d),
       ),
 
-      body: StreamBuilder(
-        stream:  Firestore.instance.collection('Animal').snapshots(),
-    builder: (context, snapshot){
-    if(!snapshot.hasData) return Text('Loading...');
-    return Column(
-      children: <Widget>[
-        Container(
-          child: Text('Name: '+ snapshot.data.documents[0]['name'],
-              style: TextStyle(
-                  fontFamily: 'Bitter',
-                  fontSize: 20.0
-              )),
-        ),
-        Container(
-          child: Text('Animal ID: '+ snapshot.data.documents[0]['animalID'],
-              style: TextStyle(
-                  fontFamily: 'Bitter',
-                  fontSize: 20.0
-              )),
-        ),
-        Container(
-          child: Text('Age: '+ snapshot.data.documents[0]['age'],
-              style: TextStyle(
-                  fontFamily: 'Bitter',
-                  fontSize: 20.0
-              )),
-        ),
-        Container(
-          child: Text('Sex: '+ snapshot.data.documents[0]['sex'],
-              style: TextStyle(
-                  fontFamily: 'Bitter',
-                  fontSize: 20.0
-              )),
-        ),
-        Container(
-          child: Text('Species: '+ snapshot.data.documents[0]['species'],
-              style: TextStyle(
-                  fontFamily: 'Bitter',
-                  fontSize: 20.0
-              )),
-        ),
-        Container(
-          child: Text('Breed: '+ snapshot.data.documents[0]['breed'],
-              style: TextStyle(
-                  fontFamily: 'Bitter',
-                  fontSize: 20.0
-              )),
-        ),
-        Container(
-          child: Text('Status: '+ snapshot.data.documents[0]['status'],
-              style: TextStyle(
-                  fontFamily: 'Bitter',
-                  fontSize: 20.0
-              )),
-        ),
-        Container(
-          child: Text('Location: '+ snapshot.data.documents[0]['location'],
-              style: TextStyle(
-                  fontFamily: 'Bitter',
-                  fontSize: 20.0
-              )),
-        ),
-        Container(
-          child: Text('Animal Picture: '+ snapshot.data.documents[0]['animalPic'],
-              style: TextStyle(
-                  fontFamily: 'Bitter',
-                  fontSize: 20.0
-              )),
-        ),
-        Container(
-          child: Text('Description: '+ snapshot.data.documents[0]['description'],
-              style: TextStyle(
-                  fontFamily: 'Bitter',
-                  fontSize: 20.0
-              )),
-        ),
-        Container(
-          child: Text('Lonely Hearts: '+ snapshot.data.documents[0]['lonelyHearts'],
-              style: TextStyle(
-                  fontFamily: 'Bitter',
-                  fontSize: 20.0
-              )),
-        ),
-        Container(
-          child: Text('Adoption Fee: '+ snapshot.data.documents[0]['adoptionFee'],
-              style: TextStyle(
-                  fontFamily: 'Bitter',
-                  fontSize: 20.0
-              )),
-        ),
-      ],
+      resizeToAvoidBottomInset: false,
 
-    );
-    }
-    ),
+      body: new SingleChildScrollView(
+      child: new StreamBuilder(
+        stream: Firestore.instance.collection('Animal').snapshots(),
+        builder: (BuildContext context, snapshot){
+          if(!snapshot.hasData) return Text('Loading...');
+          return Column(
+
+            //mainAxisAlignment:MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+
+            children: <Widget>[
+              Container(
+                child: Image.network(snapshot.data.documents[0]['_animalPic'], height: 380.0, width: 420.0,)
+              ),
+              Padding(
+                padding: EdgeInsets.only(left:0.0, right: 15.0),
+                  child:
+
+                   Text('Name: ' + snapshot.data.documents[0]['_name'],
+                      style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20.0
+                      )
+                  ),
+              ),
+
+              Padding(
+                padding: EdgeInsets.only(left:0.0, right: 15.0),
+
+                child: Text('Age: '+ snapshot.data.documents[0]['_age'],
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left:0.0, right: 15.0),
+
+                child: Text('Sex: '+ snapshot.data.documents[0]['_sex'],
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left:0.0, right: 15.0),
+
+                child: Text('Species: '+ snapshot.data.documents[0]['_species'],
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left:0.0, right: 15.0),
+
+                child: Text('Breed: '+ snapshot.data.documents[0]['_breed'],
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left:0.0, right: 15.0),
+
+                child: Text('Status: '+ snapshot.data.documents[0]['_status'],
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left:0.0, right: 15.0),
+
+                child: Text('Location: '+ snapshot.data.documents[0]['_location'],
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0
+                    )),
+              ),
+
+              Padding(
+                padding: EdgeInsets.only(left:0.0, right: 15.0),
+
+                child: Text('Description: '+ snapshot.data.documents[0]['_description'],
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left:0.0, right: 15.0),
+
+                child: Text('Lonely Hearts: '+ snapshot.data.documents[0]['_lonelyHearts'],
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left:0.0, right: 15.0),
+
+                child: Text('Adoption Fee: '+ snapshot.data.documents[0]['_adoptionFee'],
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0
+                    )),
+              ),
+
+              FlatButton(
+                color: Color(0xff6b2978),
+                onPressed: (){},
+                child: Text(
+                  'Edit Profile',
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 24.0,
+                      color: Colors.white,
+                    ),
+                ),
+              ),
+            ],
+          );
+        }
+      ),
+      ),
     );
   }
 }
+
+
